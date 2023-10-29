6c38341dad8fa2ffa25e84998b70a084a296a9b2
GeorgeSalu <george.salu10@gmail.com>
2023-10-29 14:02:52 -0300

    convidados-ng (angular) - implementando a app angular para consumir a api

---
+++ convidados-ng/src/app/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Convidado } from './convidado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  apiUrlLocal: string = 'http://localhost:8080'
+
+  constructor(private http: HttpClient) { }
+
+  getConvidados(): Observable<Convidado[]> {
+    return this.http.get<Convidado[]>(this.apiUrlLocal)
+  }
+
+}
